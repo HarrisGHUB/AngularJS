f8714bacdb01e56c5d7d34b0e9e4f9c29597c800
HarrisGHUB <92669322+HarrisGHUB@users.noreply.github.com>
2021-10-17 20:21:44 +0500

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,49 @@
+﻿using SqlToJsonToAngularJS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace SqlToJsonToAngularJS.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        db_angularJSEntities db = new db_angularJSEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult GetAllEmployee()
+        {
+            List<tbl_employee> emplist = db.tbl_employee.ToList();
+            List<employeeviewmodel> li = emplist.Select(x => new employeeviewmodel
+            {
+                emp_id = x.emp_id,
+                emp_name=x.emp_name,
+                emp_salary=x.emp_salary,
+                emp_department=x.emp_department,
+                dept_name=x.tbl_department.dept_name
+
+            }).ToList();
+            return Json(li,JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ employeeviewmodel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SqlToJsonToAngularJS.Models
+{
+    public class employeeviewmodel
+    {
+
+        public int dept_id { get; set; }
+        public string dept_name { get; set; }
+        public int emp_id { get; set; }
+        public string emp_name { get; set; }
+        public decimal emp_salary { get; set; }
+        public Nullable<int> emp_department { get; set; }
+    }
+}
